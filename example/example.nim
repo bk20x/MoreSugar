dbd1a40d89b5a09348482909bdd04c893dd41fe1
bk20x <bkaenel4@gmail.com>
2025-08-07 14:35:38 -0500

    init

---
+++ example/example.nim
@@ -0,0 +1,20 @@
+import more_sugar, json
+
+
+#basic iterator macros
+nums <- @[2,4,6,8]
+nums.each num: num -> echo
+
+
+data <- parseFile "dummy.json"
+data.elems.each elem:
+  elem.pairs k,v:
+    echo k, v
+
+
+#Sugar for return types
+proc add(a, b : int): --> int = a + b
+
+
+#Function chaining macro
+echo (25.7 + 25.5) -> toInt
---
+++ src/more_sugar.nim
@@ -0,0 +1,48 @@
+import macros
+
+
+
+
+macro `-->`*(t: typedesc): typedesc =
+   #Sugar for return type
+   t.getType() 
+
+
+
+macro each*(collection: untyped, iname: untyped, code: varargs[untyped]) =
+  result = newStmtList()
+  result.add quote do:
+    for `iname` in `collection`:
+      `code`
+
+macro pairs*(collection: untyped,kname,vname: untyped, code: varargs[untyped]) =
+  result = newStmtList()
+  result.add quote do:
+    for `kname`, `vname` in `collection`:
+      `code`
+
+
+macro `<-`*(ident, value : untyped) =
+  ## Sugar for `let`
+  result = newStmtList()
+  result.add quote do:
+    let `ident` = `value`
+
+
+
+macro `->`*(value: untyped, actions: varargs[untyped]): untyped =
+  ## Pipes `value` into a chain of actions
+  # "Hello World" -> echo
+  # (50.2 + 50.5) -> toInt
+  var currentResult = value
+  for action in actions:
+    if action.kind == nnkStmtListExpr:
+      let body = action[0]
+      currentResult = quote do:
+        let it = `currentResult`
+        `body`
+    else:
+      currentResult = quote do:
+        `action` `currentResult`
+  result = currentResult
+
