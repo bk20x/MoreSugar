1dd52593045214f9db09fbfc5412321ffc442401
bk20x <bkaenel4@gmail.com>
2025-09-03 06:56:08 -0500

    moving stuff around

--- src/more_sugar.nim
+++ src/more_sugar.nim
@@ -7,42 +7,33 @@ import macros
 
 
 macro each*(collection: untyped, iname: untyped, code: varargs[untyped]) =
+  ## Loop over `iname` being the yield of `items collection`
   result = newStmtList()
   result.add quote do:
-    for `iname` in `collection`:
+    for `iname` in items `collection`:
       `code`
 
 macro pairs*(collection: untyped,kname,vname: untyped, code: varargs[untyped]) =
+  ## Loop over `kname`, `vname` being the keys, values of `collection`
   result = newStmtList()
   result.add quote do:
     for `kname`, `vname` in `collection`:
       `code`
 
 macro `->`*(t: typedesc): typedesc =
-   #Sugar for return type
+   ## Sugar for return type as `proc *NAME*(): -> t`
    t.getType() 
 
 macro `<-`*(ident, value : untyped) =
-  ## Sugar for `let` being the form `let ,ident = ,value`
+  ## Sugar for `let` being the form `let ident = value`
   result = newStmtList()
   result.add quote do:
     let `ident` = `value`
 
 
 
-macro `->`*(value: untyped, actions: varargs[untyped]): untyped =
-  ## Pipes `value` into a chain of actions
-  # "Hello World" -> echo
-  # (50.2 + 50.5) -> toInt
-  var currentResult = value
-  for action in actions:
-    if action.kind == nnkStmtListExpr:
-      let body = action[0]
-      currentResult = quote do:
-        let it = `currentResult`
-        `body`
-    else:
-      currentResult = quote do:
-        `action` `currentResult`
-  result = currentResult
+macro `->`*(what: untyped, expr: untyped): untyped =
+  ## Pipes `what` into `actions` being the form `expr what`
+  quote do:
+    `action` `what`
 
